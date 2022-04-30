870090844c65f80ca6786b9947d02a1bee19808f
Paulo Rayann <paulorpsena@gmail.com>
2022-04-30 15:32:14 -0300

    style:  format to accept pt-br date style in POST

--- models/users.js
+++ models/users.js
@@ -1,11 +1,18 @@
+const moment = require('moment')
+
 const connection = require('../database/connection')
+
 
 class Users {
     create(users) {
+        const birthDate = moment(users.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        const bDate = {...users, birthDate}
+
+
         const sql = 'INSERT INTO Users SET ?'
 
 
-        connection.query(sql, users, (err, outcome) => {
+        connection.query(sql, bDate, (err, outcome) => {
             if(err) {
                 console.log(err)
             } else {
