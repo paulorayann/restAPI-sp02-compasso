f814d7f4d7ca46ae07c2de604d6862090376fbf9
Paulo Rayann <paulorpsena@gmail.com>
2022-04-29 21:54:36 -0300

    style: improving query formatting

--- database/tables.js
+++ database/tables.js
@@ -6,7 +6,23 @@ class Tables {
     }
 
     createUser() {
-        const sql = 'CREATE TABLE Users (id int NOT NULL AUTO_INCREMENT, name varchar(70) NOT NULL, cpf varchar(11) NOT NULL, birthDate DATE NOT NULL, email varchar(50) NOT NULL, password varchar(30) NOT NULL, address varchar(200) NOT NULL, number varchar(15) NOT NULL, complement varchar(60) NOT NULL, city varchar(50) NOT NULL, state varchar(25) NOT NULL, country varchar(55) NOT NULL, zipCode varchar(8) NOT NULL, PRIMARY KEY (id))'
+        const sql = ` 
+        CREATE TABLE Users (
+        id int NOT NULL AUTO_INCREMENT,
+        name varchar(70) NOT NULL,
+        cpf varchar(11) NOT NULL,
+        birthDate DATE NOT NULL,
+        email varchar(50) NOT NULL,
+        password varchar(30) NOT NULL,
+        address varchar(200) NOT NULL,
+        number varchar(15) NOT NULL,
+        complement varchar(60) NOT NULL,
+        city varchar(50) NOT NULL,
+        state varchar(25) NOT NULL,
+        country varchar(55) NOT NULL,
+        zipCode varchar(8) NOT NULL,
+        PRIMARY KEY (id))
+        `
 
         this.connection.query( sql, err => {
             if(err) {
