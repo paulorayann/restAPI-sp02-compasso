4b10cf8a64cef9186fc2ccced90ec9acdfafa77a
Paulo Rayann <paulorpsena@gmail.com>
2022-04-30 23:11:44 -0300

    feat: Get by ID and Post Method

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,22 @@
 const Users = require('../models/users')
 
 module.exports = app => {
-    app.get('/users', (req, res) => res.send('GET request in the "Users" route'))
 
-    app.post('/users', (req, res) => {
+    //GET Method
+    app.get('/api/v1/user', (req, res) =>  {
+        Users.read(res)
+
+    })
+    //GET by ID
+    app.get('/api/v1/user/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Users.readById(id, res)
+    })
+
+
+
+    app.post('/api/v1/user', (req, res) => {
         const users = req.body
 
         Users.create(users, res)
--- models/users.js
+++ models/users.js
@@ -4,10 +4,11 @@ const connection = require('../database/connection')
 
 
 class Users {
+    //Create Method
     create(users, res) {
-        const birthDate = moment(users.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        const birthDate = moment(users.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD')
 
-        const validBirthDate = moment(birthDate).isBefore('2004-01-01', 'year')
+        const validBirthDate = moment(birthDate).isBefore('2004-01-01')
         const validPassword = users.password.length >= 6
 
         const validation = [
@@ -46,7 +47,37 @@ class Users {
 
 
 
+    } //End of Create Method
+
+    //Read Method
+    read(res) {
+        const sql = 'SELECT * FROM Users'
+
+        connection.query(sql, (err, outcome) => {
+            if(err) {
+                res.status(404).json(err)
+            } else {
+                res.status(200).json(outcome)
+            }
+
+        })
+
     }
+
+    readById(id, res) {
+        const sql = `SELECT * FROM Users WHERE id=${id}`
+    
+        connection.query(sql, (err, outcome) => {
+            const user = outcome[0]
+            if(err) {
+                res.status(404).json(err)
+            } else {
+                res.status(200).json(user)
+            }
+
+        })
+    }
+    
 }
 
 module.exports = new Users
